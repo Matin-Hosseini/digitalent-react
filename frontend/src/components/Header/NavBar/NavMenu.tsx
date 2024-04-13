0fcc903f5fc65f29fab7f0c50ec6ccd19376aff4
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-04-13 10:35:03 +0330

    frontend: nav menu data changed and drawer added

--- frontend/src/components/Header/NavBar/NavMenu.tsx
+++ frontend/src/components/Header/NavBar/NavMenu.tsx
@@ -1,14 +1,41 @@
+import { IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import { MdOutlineMenu } from "react-icons/md";
 import SideBar from "../SideBar";
 import NavLink from "./NavLink";
 
 const navMenu = [
-  { id: 1, title: "خانه", megaMenu: false, to: "/" },
-  { id: 2, title: "دوره ها", megaMenu: true, to: "/courses" },
-  { id: 3, title: "برنامه نویسی", megaMenu: false, to: "/courses/programming" },
-  { id: 4, title: "مقالات", megaMenu: false, to: "/articles" },
-  { id: 5, title: "ارتباط با ما", megaMenu: false, to: "/contact-us" },
+  {
+    id: 1,
+    title: "خانه",
+    path: "/",
+    submenu: [{ id: "fsdknfs", title: "زیر منوی 1" }],
+  },
+  {
+    id: 2,
+    title: "دوره ها",
+    path: "/courses",
+    megamenu: [
+      { id: "fnee", title: "فیلمبرداری" },
+      { id: "fege", title: "گرافیک" },
+      { id: "leme", title: "آشپزی" },
+      { id: "fefe", title: "ui/ux" },
+      { id: "aaae", title: "انیمیشن سازی" },
+    ],
+  },
+  {
+    id: 3,
+    title: "برنامه نویسی",
+    path: "/courses/programming",
+    submenu: [
+      { id: "fnslkf", title: "فرانت اند", path: "/courses/frontend" },
+      { id: "febe", title: "بک اند", path: "/courses/backend" },
+      { id: "hehrh", title: "موبایل", path: "/courses/mobile" },
+      { id: "ehhjy", title: "دیتا ساینس", path: "/courses/datasience" },
+    ],
+  },
+  { id: 4, title: "مقالات", path: "/articles" },
+  { id: 5, title: "ارتباط با ما", path: "/contact-us" },
 ];
 
 export default function NavMenu() {
@@ -28,11 +55,20 @@ export default function NavMenu() {
         ))}
       </ul>
 
-      <button className="block lg:hidden" onClick={() => setShowSideBar(true)}>
-        <MdOutlineMenu />
-      </button>
-
-      <SideBar show={showSideBar} onHide={() => setShowSideBar(false)} />
+      <div className="lg:hidden">
+        <IconButton
+          className="block lg:hidden"
+          onClick={() => setShowSideBar(true)}
+        >
+          <MdOutlineMenu className="text-color" />
+        </IconButton>
+      </div>
+      
+      <SideBar
+        show={showSideBar}
+        onHide={() => setShowSideBar(false)}
+        menus={menus}
+      />
     </>
   );
 }
