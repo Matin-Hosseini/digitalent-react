ffb5b2f94c3c514ccfdb973e2c560a1cd2eab37e
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-04-13 10:44:08 +0330

    frontend: update: navbar

--- frontend/src/components/Header/NavBar/NavLink.tsx
+++ frontend/src/components/Header/NavBar/NavLink.tsx
@@ -2,44 +2,49 @@ import { Link, NavLink as RouterNavLink } from "react-router-dom";
 
 export default function NavLink({
   title,
-  megaMenu,
-  to,
+  megamenu,
+  submenu,
+  path,
 }: {
   title: String;
-  megaMenu: boolean;
-  to: string;
+  megamenu: boolean;
+  path: string;
 }) {
   return (
-    <li className={`h-full block  group ${!megaMenu ? "relative" : ""}`}>
+    <li className={`h-full block  group ${!megamenu ? "relative" : ""}`}>
       <RouterNavLink
-        to={to}
+        to={path}
         className={({ isActive }) => {
           return [
-            `relative inline-block h-full leading-[50px] overflow-hidden  group-hover:text-[var(--red)]  transition-all before:absolute before:content-[""] before:w-full before:h-[8px] before:bg-[var(--red)] before:bottom-[-5px] before:rounded-lg before:translate-y-10 before:rotate-[-4deg]  group-hover:before:translate-y-0`,
-            isActive
-              ? "before:translate-y-0 before:rotate-[-4deg] text-[var(--red)]"
-              : "",
+            `relative inline-block h-full leading-[50px] overflow-hidden  group-hover:text-[var(--red)]  transition-all before:absolute before:content-[""] before:w-full before:h-[8px] before:bg-[var(--red)] before:bottom-[-5px] before:rounded-lg before:translate-y-10  before:rotate-[-4deg]  group-hover:before:translate-y-0`,
+            isActive ? "before:translate-y-0 text-[var(--red)]" : "",
           ].join(" ");
         }}
       >
         {title}
       </RouterNavLink>
 
-      <div
+      {megamenu && (
+        <div
         className={`absolute top-full right-0 z-50 flex flex-col flex-wrap items-start ${
-          megaMenu ? "w-full" : "w-[30rem]"
+          megamenu ? "w-full" : "w-[30rem]"
         } gap-3 p-4 transition-all bg-[var(--section-bg)] max-h-64 border border-t-2 border-[var(--red)] shadow-lg rounded-lg invisible opacity-0 group-hover:opacity-100 group-hover:visible`}
       >
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
-        <Link to={"/"}>زیر منو</Link>
+        {megamenu.map(menu => (
+          <Link to={"/"} key={menu.id}>{menu.title}</Link>
+        ))}
       </div>
+      )}
+
+      {submenu && (
+        <div
+          className={`absolute top-full right-0 z-50 flex flex-col flex-wrap items-start w-[30rem] gap-3 p-4 transition-all bg-[var(--section-bg)] max-h-64 border border-t-2 border-[var(--red)] shadow-lg rounded-lg invisible opacity-0 group-hover:opacity-100 group-hover:visible`}
+        >
+          {submenu.map(menu => (
+            <Link to={"/"} key={menu.id}>{menu.title}</Link>
+          ))}
+        </div>
+      )}
     </li>
   );
 }
