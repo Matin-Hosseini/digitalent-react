804536f8d10ac63bb8b0d05c7e43e312b1c68924
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-04-14 04:29:24 +0330

    frontend: side bar made responsive and menu array created and applied

---
+++ frontend/src/pages/User-panel/Sidebar.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import {
+  RiCoupon2Line,
+  RiDashboardLine,
+  RiDatabase2Line,
+  RiEditLine,
+  RiKeyLine,
+  RiLogoutCircleRLine,
+  RiMedalLine,
+  RiMoneyDollarCircleLine,
+} from "react-icons/ri";
+import PanelSidebarLink from "./SidebarLink";
+
+const userPanelSidebarMenu = [
+  {
+    id: "nekjn",
+    title: "داشبورد",
+    icon: <RiDashboardLine />,
+    path: "dashboard",
+  },
+  {
+    id: "enbr",
+    title: "ویرایش حساب کاربری",
+    icon: <RiEditLine />,
+    path: "edit-account",
+  },
+  {
+    id: "wevwe",
+    title: "تغییر رمز عبور",
+    icon: <RiKeyLine />,
+    path: "change-password",
+  },
+  {
+    id: "vevg",
+    title: "دوره های من",
+    icon: <RiDatabase2Line />,
+    path: "courses",
+  },
+  {
+    id: "evea",
+    title: "داشبورد",
+    icon: <RiMoneyDollarCircleLine />,
+    path: "buyed-courses",
+  },
+  {
+    id: "feve",
+    title: "داشبورد",
+    icon: <RiMedalLine />,
+    path: "free-courses",
+  },
+  {
+    id: "avae",
+    title: "تیکت ها",
+    icon: <RiCoupon2Line />,
+    path: "tickets",
+  },
+  {
+    id: "evehh",
+    title: "خروج از حساب",
+    icon: <RiLogoutCircleRLine />,
+    path: "get-out",
+  },
+];
+
+export default function PanelSidebar() {
+  const [sidebarMenu, setSidebarMenu] = useState([]);
+
+  useEffect(() => {
+    setSidebarMenu(userPanelSidebarMenu);
+  }, []);
+
+  return (
+    <div className="account-side-bar">
+      <div className="profile"></div>
+
+      <div className="menu">
+        {sidebarMenu.map((menu) => (
+          <PanelSidebarLink key={menu.id} {...menu} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/User-panel/SidebarLink.tsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+export default function PanelSidebarLink({ path, icon, title }) {
+  return (
+    <NavLink
+      to={path}
+      className={({ isActive }) => {
+        return [
+          "flex items-centerr gap-3 p-3 rounded-lg hover:text-[var(--purple)] hover:bg-[var(--bg-purple)]",
+          isActive ? "bg-[var(--bg-purple)] text-[var(--purple)]" : "",
+        ].join(" ");
+      }}
+    >
+      {icon}
+      {title}
+    </NavLink>
+  );
+}
