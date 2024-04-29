f08b1a8796e4152707d4527bf238107a2a6dd1ea
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-04-29 07:13:53 +0330

    backend: category CRUD api created

--- backend/src/api/v1/controllers/category.js
+++ backend/src/api/v1/controllers/category.js
@@ -1,11 +1,75 @@
 const { Category } = require("./../../../models");
 
-const createCategory = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+/*
+ * Returns all of the categories
+ * /category
+ * public
+ */
+const getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.findAll();
+  return res.status(200).json(categories);
+});
+
+/*
+ * Inserts a new category into database
+ * /category/create
+ * private(admin)
+ */
+const createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
-  const newCategory = await Category.create({ name });
 
-  res.status(201).json({ message: "New Category created!", newCategory });
+  const category = await Category.findOne({
+    where: {
+      name,
+    },
+  });
+
+  if (category) return res.status(400).json({ err: "Category already exists" });
+
+  await Category.create({ name });
+
+  res.status(201).json({ message: "New Category created!" });
+});
+
+/*
+ * Updates the existing category
+ * /category/update?categoryId=(UUID)
+ * private(admin)
+ */
+const updateCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findOne({
+    where: {
+      id: req.query.categoryId,
+    },
+  });
+
+  if (!category) return res.status(404).json({ err: "Category not found" });
+
+  category.name = req.body.name;
+  await category.save();
+  res.status(200).json({ msg: "category updated successfully" });
+});
+
+/*
+ * Deletes the existing category
+ * /category/delete?categoryId=(UUID)
+ * private(admin)
+ */
+const deleteCategory = asyncHandler(async (req, res) => {
+  await Category.destroy({
+    where: {
+      id: req.query.categoryId,
+    },
+  });
+
+  res.status(200).json({ msg: "Category removed successfully" });
+});
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getAllCategories,
 };
-
-
-module.exports = {createCategory}
--- backend/src/routes/v1/category.js
+++ backend/src/routes/v1/category.js
@@ -3,6 +3,9 @@ const categoryController = require("./../../api/v1/controllers/category");
 
 const router = express.Router();
 
+router.get("/", categoryController.getAllCategories);
 router.post("/create", categoryController.createCategory);
+router.put("/update", categoryController.updateCategory);
+router.delete("/delete", categoryController.deleteCategory);
 
-module.exports = router
+module.exports = router;
