055e149e5878a6b454e0466442b1781aeeeaae4a
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-05-02 10:55:00 +0330

    frontend: video player

--- frontend/src/components/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer.tsx
@@ -1,33 +1,130 @@
-import { useRef } from "react";
-import VideoJS from "./VideoPlayer";
+import ReactPlayer from "react-player";
+
+import { useState } from "react";
+
+import "./VideoPlayer.css";
+
+import { Slider, styled } from "@mui/material";
+
+import {
+  FastForward,
+  FastRewind,
+  Pause,
+  PlayArrow,
+  SkipNext,
+  VolumeUp,
+} from "@mui/icons-material";
+
+const Control = ({ onPlay }) => {
+  // const useStyles = makeStyles({
+  //   volumeSlider: {
+  //     width: "100px",
+  //     color: "#9556CC",
+  //   },
+
+  //   bottomIcons: {
+  //     color: "#999",
+  //     padding: "12px 8px",
+
+  //   "&:hover": {
+  //       color: "#fff",
+  //     },
+  //   },
+  //  });
+
+  const PrettoSlider = styled(Slider)({
+    root: {
+      height: "20px",
+      color: "#9556CC",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    thumb: {
+      height: 20,
+      width: 20,
+      backgroundColor: "#9556CC",
+      border: "2px solid currentColor",
+      marginTop: -3,
+      marginLeft: -12,
+      "&:focus, &:hover, &$active": {
+        boxShadow: "inherit",
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: "calc(-50% + 4px)",
+    },
+    track: {
+      height: 5,
+      borderRadius: 4,
+      width: "100%",
+    },
+    rail: {
+      height: 5,
+      borderRadius: 4,
+    },
+  });
+
+  return (
+    <>
+      <div className="top_container">
+        <h2>Video PLayer</h2>
+      </div>
+      {/* middle container */}
+      <div className="mid__container">
+        <div className="icon__btn">
+          <FastForward fontSize="medium" />
+        </div>
+
+        <div className="icon__btn" onClick={() => onPlay()}>
+          <Pause fontSize="medium" />
+        </div>
+        <div className="icon__btn">
+          <FastRewind fontSize="medium" />
+        </div>
+      </div>
+      {/* bottom container */}
+      <div className="bottom__container">
+        <div className="slider__container">
+          <PrettoSlider />
+        </div>
+        <div className="control__box">
+          <div className="inner__controls">
+            <div className="icon__btn">
+              <PlayArrow fontSize="medium" />
+            </div>
+            <div className="icon__btn">
+              <SkipNext fontSize="medium" />
+            </div>
+            <div className="icon__btn">
+              <VolumeUp fontSize="medium" />
+            </div>
+
+            <Slider className={``} />
+            <span>5/20</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default function VideoPlayer() {
-  const playerRef = useRef(null);
+  const [play, setPlay] = useState(false);
+  return (
+    <ReactPlayer
+      className="player"
+      url="src/components/test.mp4"
+      // url="https://www.youtube.com/watch?v=9DqW9R8Lfmw"
+      width="100%"
+      height="100%"
+      controls
+      playing={play}
+    />
+  );
+}
 
-  const videoJsOptions = {
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [
-      {
-        src: "src/assets/videos/course/video (720p).mp4",
-        type: "video/mp4",
-      },
-    ],
-  };
-
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
-
-    // You can handle player events here, for example:
-    player.on("waiting", () => {
-      videojs.log("player is waiting");
-    });
-
-    player.on("dispose", () => {
-      videojs.log("player will dispose");
-    });
-  };
-  return <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />;
+{
+  /* <Control onPlay={() => setPlay((prev) => !prev)} /> */
 }
