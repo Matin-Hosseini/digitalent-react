8743c0edd40c31bb3e6663dc5ea931057c896d54
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-05-04 21:24:17 +0330

    frontend: bugfix for theme context

--- frontend/src/contexts/theme.tsx
+++ frontend/src/contexts/theme.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import useLocalStorage from "../hooks/localstorage";
 
 type theme = "dark" | "light";
@@ -11,13 +11,13 @@ type themeContext = {
 
 export const ThemeContext = createContext<themeContext | null>(null);
 
-const ThemeProvider = ({ children }: { children: ReactElement }) => {
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<theme>("light");
   const [localStoragetheme, setLocalStorageTheme] = useLocalStorage("theme");
 
   const toggleTheme = () => {
     const newTheme: theme = theme === "light" ? "dark" : "light";
-    
+
     setTheme(newTheme);
     setLocalStorageTheme(newTheme);
   };
