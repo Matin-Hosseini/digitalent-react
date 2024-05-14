5554ed19e16bc31961c0db9e2e2439a3bc66402d
MATIN\Asus <matinhosseiniofficial@gmail.com>
2024-05-15 00:56:08 +0330

    frontend: blob box update

--- frontend/src/components/BlogBox.tsx
+++ frontend/src/components/BlogBox.tsx
@@ -3,35 +3,36 @@ import ContainerBox from "./ContainerBox";
 
 //react-icons
 
-export default function BlogBox() {
+export default function BlogBox({
+  id,
+  cover,
+  title,
+  views,
+  subtitle,
+  author,
+  short_name,
+  createdAt,
+}) {
+  
   return (
     <>
       <ContainerBox className="p-3">
         <article>
-          <Link to="/article">
-            <img
-              src="src/assets/images/6a1e69a6-6922-43ab-bc85-5d8548a32e20_cover_thumb.webp"
-              alt=""
-              className="rounded-lg mb-4"
-            />
+          <Link to={`/article/${short_name}`}>
+            <img src={cover} alt="" className="rounded-lg mb-4 h-[16rem]" />
           </Link>
-          <p className="text-[var(--yellow)] text-base">12 اردیبهشت 1402</p>
+          <p className="text-[var(--yellow)] text-base">{createdAt}</p>
           <h2 className="text-2xl line-clamp-2 mt-3 mb-6 font-bold h-[4rem]">
-            <Link to="/article">
-              این متن به صورت آزمایشی قرار گرفته است این متن به صورت ازمایشی
-              قرار گرفته است
-            </Link>
+            <Link to={`/article/${short_name}`}>{title}</Link>
           </h2>
           <p className="text-base text-[var(--gray)] mb-5 line-clamp-3 h-[4.5rem]">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-            استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
-            ستون و سطرآنچنان که لازم است
+            {subtitle}
           </p>
           <div className="flex justify-end">
             <div className="flex items-center gap-3">
               <div className="text-left">
-                <h3 className="text-base">ریحانه حدادی</h3>
-                <p className="text-sm text-[var(--gray)]">10 مرداد 1396</p>
+                <h3 className="text-base">{author.name}</h3>
+                <p className="text-sm text-[var(--gray)]">{author.joined_on}</p>
               </div>
               <img
                 src="src/assets/images/profile.jpg"
